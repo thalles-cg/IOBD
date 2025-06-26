5db8ace7f8d7873a0594fed7bbef564ba63d4d38
thalles-cg <thalles.pity@gmail.com>
2025-06-26 19:58:15 -0300

    feat: Adição da classe de conexão e configuração do pom

--- edulivre/src/main/java/apresentacao/Main.java
+++ edulivre/src/main/java/apresentacao/Main.java
@@ -1,7 +1,12 @@
 package apresentacao;
 
+import java.sql.Connection;
+
+import persistencia.ConexaoPostgreSQL;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        Connection con = new ConexaoPostgreSQL().getConnection();
     }
 }
--- edulivre/src/main/java/persistencia/ConexaoPostgreSQL.java
+++ edulivre/src/main/java/persistencia/ConexaoPostgreSQL.java
@@ -1,17 +1,31 @@
 package persistencia;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConexaoPostgreSQL {
     private String host;
     private String user;
     private String password;
     private int port;
-    private String dbname;
+    private String database;
 
     public ConexaoPostgreSQL(){
         this.host = "localhost";
         this.user = "postgres";
         this.password = "123";
         this.port = 5432;
-        this.dbname = "edulivre";
+        this.database = "edulivre";
+    }
+
+    public Connection getConnection(){
+        String url = "jdbc:postgresql://"+ this.host + ":"+this.port+"/"+this.database;
+        try {
+            return DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            System.err.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+        }
+        return null;
     }
 }
